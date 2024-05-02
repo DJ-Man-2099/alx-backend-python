fc3544d4719a5eedbebff8984964486540795f5c
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-02 15:41:12 +0300

    Task 6

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""6th Task"""
+
+
+import math
+
+
+def sum_list(input_list: list[float]) -> float:
+    """takes a list input_list of floats as argument
+    and returns their sum as a float."""
+    return math.fsum(input_list)
