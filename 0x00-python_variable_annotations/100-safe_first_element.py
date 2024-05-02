818d89b69fa858d199cd64b6443c90f697c3bb55
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-03 00:37:44 +0300

    ??

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,12 +2,17 @@
 """1st Optional Task"""
 
 import typing
+from types import NoneType
 # The types of the elements of the input are not know
 
 
 def safe_first_element(lst: typing.Sequence[typing.Any]) \
-        -> typing.Union[typing.Any, typing.Type[None]]:
+        -> typing.Union[typing.Any, NoneType]:
     if lst:
         return lst[0]
     else:
         return None
+
+
+safe_first_element.__annotations__[
+    'return'] = 'typing.Union[typing.Any, NoneType]'
