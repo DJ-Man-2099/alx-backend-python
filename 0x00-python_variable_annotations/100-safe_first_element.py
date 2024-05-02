a9682eaf2ae138cc8402eebb5d8d09104482443d
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-03 00:58:59 +0300

    Optional Task 1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-'''
-type-annotated function
-'''
-from typing import Any, Sequence, Union
+"""1st Optional Task"""
+
+import typing
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''Retrieves the first element of a sequence if it exists.
-    '''
+def safe_first_element(lst: typing.Sequence[typing.Any]) \
+        -> typing.Union[typing.Any, None]:
+    """Return the first element of a sequence"""
     if lst:
         return lst[0]
     else:
