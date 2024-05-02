ecae762f1efec87b5485a52c59dee3451fbdd7a1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-03 01:01:38 +0300

    Optional Task 2

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """2nd Optional Task"""
 
-import typing
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
 
 
-def safely_get_value(dct: typing.Mapping, key: typing.Any, default=None):
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) \
+        -> Union[Any, T]:
+    """Return the value of a key in a dictionary or the default value"""
     if key in dct:
         return dct[key]
     else:
