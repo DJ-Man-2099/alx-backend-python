38d34550ba6eeaa5ddce861abc1dc10330cda755
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-25 01:42:13 +0300

    Task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Testing Utils for Github ORG"""
 
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, PropertyMock, patch
 from parameterized import parameterized
 from client import GithubOrgClient, get_json
 import client
@@ -23,3 +23,11 @@ class TestGithubOrgClient(unittest.TestCase):
         test_object = GithubOrgClient(org_name)
         self.assertEqual(test_object.org, correct_result)
         self.assertTrue(mock_get_json.called)
+
+    def test_public_repos_url(self):
+        """method to unit-test GithubOrgClient._public_repos_url"""
+        test_object = GithubOrgClient(org_name="Test")
+        with patch.object(GithubOrgClient, "org",
+                          new_callable=PropertyMock) as property_mock:
+            property_mock.return_value = {"repos_url": "Test"}
+            self.assertEqual(test_object._public_repos_url, "Test")
