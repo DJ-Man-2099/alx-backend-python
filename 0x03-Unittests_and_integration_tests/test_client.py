13821357933c6fde3ebcfc8fc2891c2f95797505
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-25 03:06:30 +0300

    Task 9 Now?

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -74,9 +74,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def setUpClass(cls):
         """mock requests.get to
          return example payloads found in the fixtures"""
-        result = [cls.org_payload, cls.repos_payload,]*2
+        result = cls.repos_payload
         return_mock = MagicMock()
-        return_mock.json = MagicMock(return_value=result)
+        return_mock.json = MagicMock(side_effect=result)
         cls.get_patcher = patch.object(
             requests, "get",
             return_value=return_mock)
